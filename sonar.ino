1542d8f1cd1a66863b6216c34ed3b5d4ecb73424
Priya5346 <112251873+Priya5346@users.noreply.github.com>
2023-10-18 23:32:39 +0530

    Update sonar.ino

--- sonar.ino
+++ sonar.ino
@@ -1,26 +1,20 @@
 int trigPin1=2;
 int echoPin1=3;
 
-
 int trigPin2=4;
 int echoPin2=5;
-
 
 int trigPin3=6;
 int echoPin3=7;
 
-
 int trigPin4=8;
 int echoPin4=9;
-
 
 int trigPin5=10;
 int echoPin5=11;
 
-
 //int Buzzer = 13; // Connect buzzer pin to 13
 int ledPin= 12;  //Connect LEd pin to 12
-
 
 void setup() {
   Serial.begin (9600);
@@ -37,14 +31,11 @@ void setup() {
   //pinMode(Buzzer, OUTPUT);
   pinMode(ledPin, OUTPUT);
 
-
 }
-
 
 void loop() {
   long duration1, distance1;
   int var1=0,var2=0,var3=0,var4=0,var5=0,sum,count=0;
-
 
   digitalWrite(trigPin1, LOW);  // Added this line
   delayMicroseconds(2); // Added this line
@@ -53,7 +44,6 @@ void loop() {
   digitalWrite(trigPin1, LOW);
   duration1 = pulseIn(echoPin1, HIGH);
   distance1 = (duration1/2) / 29.1;
-
 
   if (distance1 >= 500 || distance1 <= 0){
     // Serial.println("Out of range");
@@ -76,7 +66,6 @@ void loop() {
    duration2 = pulseIn(echoPin2, HIGH);
    distance2= (duration2/2) / 29.1;
 
-
    if (distance2 >= 500 || distance2 <= 0){
     // Serial.println("Out of range");
     var2=0;
@@ -98,7 +87,6 @@ void loop() {
   duration3 = pulseIn(echoPin3, HIGH);
   distance3= (duration3/2) / 29.1;
 
-
    if (distance3 >= 500 || distance3 <= 0){
     // Serial.println("Out of range");
     var3=0;
@@ -112,7 +100,6 @@ void loop() {
   }
   //delay(20);
 
-
   long duration4, distance4;
   digitalWrite(trigPin4, LOW);  // Added this line
   delayMicroseconds(2); // Added this line
@@ -121,7 +108,6 @@ void loop() {
   digitalWrite(trigPin4, LOW);
   duration4 = pulseIn(echoPin4, HIGH);
   distance4= (duration4/2) / 29.1;
-
 
    if (distance4 >= 500 || distance4 <= 0){
     // Serial.print("Out of range");
@@ -137,8 +123,6 @@ void loop() {
   //delay(20);
 
 
-
-
   long duration5, distance5;
   digitalWrite(trigPin5, LOW);  // Added this line
   delayMicroseconds(2); // Added this line
@@ -147,7 +131,6 @@ void loop() {
   digitalWrite(trigPin5, LOW);
   duration5 = pulseIn(echoPin5, HIGH);
   distance5= (duration5/2) / 29.1;
-
 
    if (distance5 >= 500 || distance5 <= 0){
     // Serial.println("Out of range");
@@ -162,7 +145,6 @@ void loop() {
   }
   //delay(20);
 
-
   if(count==0){
     sum=0;
   }
@@ -172,9 +154,8 @@ void loop() {
   Serial.print("Distance is ");
   Serial.print(sum);
   Serial.println("cm");
- // Serial.print(count);
- 
-  if (sum >=0 && sum <= 10)
+  
+  if (sum >=0 && sum <= 10) 
         {
         //tone(Buzzer,400);              // play tone of 400Hz for 500 ms
         digitalWrite(ledPin,HIGH);
